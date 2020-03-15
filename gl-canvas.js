4b38dffa13f23583a1a8b6e5938382c83d42b6d7
Shun Git <kwokshungit@gmail.com>
2020-03-15 22:07:04 +0800

    extract core functionality into base

--- gl-canvas.js
+++ gl-canvas.js
@@ -1,327 +1,56 @@
-const STYLE = `
-  :host { display: block }
-  :host([hidden]) { display: none }
-
-  canvas {
-    width: 100%;
-    height: 100%;
-    display: block;
-  }`
-
-const BOOL_ATTRS = [
-  'alpha',
-  'depth',
-  'stencil',
-  'antialias',
-  'desynchronized',
-]
-
-function getAlphaType (pref) {
-  const premultipliedAlpha = (pref === 'premultiplied')
-  const alpha = (pref === 'straight' || premultipliedAlpha)
-  return { alpha, premultipliedAlpha }
-}
-
-function getPowerPreference (pref) {
-  switch (pref) {
-    case 'low-power':
-    case 'high-performance':
-      return pref
-    default:
-      return 'default'
-  }
-}
-
-class ResizeEvent extends UIEvent {
-  constructor (name, entry) {
-    super(name, { bubbles: false })
-    this.entry = entry
-    this.size = entry.target.size
-  }
-}
+import { GLCanvasBase } from './src/base.js'
 
 /**
  * `<gl-canvas>` is a wrapper around `<canvas>` that handles the WebGL context and various events.
  */
-export class GLCanvas extends HTMLElement {
-  constructor () {
-    super()
-
-    this._gl = null
-    this._requestId = null
-    this._contextLost = false
-
-    this._contextAttributes = {}
-    this._pixelRatio = window.devicePixelRatio
-
-    this._setup = null
-    this._render = null
-    this._resize = null
-    this._dispose = null
-
-    this._ctxLost = (e) => {
-      e.preventDefault()
-      cancelAnimationFrame(this._requestId)
-      console.info('[GLCanvas] Context lost')
-      this._contextLost = true
-    }
-
-    this._ctxRestored = () => {
-      console.info('[GLCanvas] Context restored')
-      this._contextLost = false
-      this._initGL(true)
-    }
-
-    this._canvas = document.createElement('canvas')
-
-    const shadow = this.attachShadow({ mode: 'closed' })
-    shadow.appendChild(this.constructor.style)
-    shadow.appendChild(this._canvas)
-  }
-
-  static get style () {
-    if (!this._style) {
-      this._style = document.createElement('style')
-      this._style.textContent = STYLE
-    }
-    return this._style
-  }
-
-  static get resizeObserver () {
-    if (!this._resizeObserver) {
-      this._resizeObserver = new ResizeObserver((entries) => {
-        for (const entry of entries) {
-          if (entry.target._contextLost) { continue }
-          const evt = new ResizeEvent('resize', entry)
-          if (entry.target.dispatchEvent(evt)) {
-            const canvas = entry.target._canvas
-            const [width, height] = entry.target.size
-            if (canvas.width !== width || canvas.height !== height) {
-              canvas.width = width
-              canvas.height = height
-            }
-          }
-        }
-      })
-    }
-    return this._resizeObserver
-  }
-
+export class GLCanvas extends GLCanvasBase {
   /**
-   * The width and height of the canvas after pixel scaling.
-   * @type {Array.<number>}
+   * Run setup code and initialize draw function.
    */
-  get size () {
-    return [
-      this._canvas.clientWidth * this.pixelRatio,
-      this._canvas.clientHeight * this.pixelRatio,
-    ]
+  setupCallback (restored) {
+    if (this.gl === null || !this._setup) { return }
+    const draw = this._setup(this.gl, restored)
+    if (draw === null || typeof draw === 'function') {
+      this._draw = draw
+    }
+  }
+
+  /*
+   * Sets viewport bounds to match canvas.
+   */
+  resizedCallback (rect) {
+    super.resizedCallback()
+    if (!this._contextLost) {
+      this.gl.viewport(0, 0, rect.width, rect.height)
+    }
   }
 
   /**
-   * Setup function. Called on initialize and when WebGL context is restored.
+   * Define WebGL setup function.
    * @callback setupFunction
    * @param {?WebGLRenderingContext} gl - The current WebGL context.
-   * @param {?boolean} restored - Was context restored? null if setup was changed.
+   * @param {boolean} restored - Was context restored?
+   * @return {?function} Draw function, or null to unset.
    *
-   * @type {?setupFunction}
+   * @param {setupFunction}
    */
-  get setup () {
-    return this._setup
-  }
-
-  set setup (v) {
-    this._setup = (typeof v === 'function') ? v : null
-    this._initGL(null)
+  init (setup) {
+    if (typeof setup !== 'function') { return }
+    this._setup = setup
+    this.setupCallback(false)
   }
 
   /**
-   * Animation loop function.
-   * @callback renderFunction
-   * @param {DOMHighResTimeStamp} time - Timestamp in milliseconds.
+   * Run draw function
+   * @param {...*} args - Arguments for the draw function.
    *
-   * @type {?renderFunction}
+   * @return {?object} Null if draw was not run.
    */
-  get render () {
-    return this._render
-  }
-
-  set render (v) {
-    if (v === undefined) { return }
-    cancelAnimationFrame(this._requestId)
-    this._render = (typeof v === 'function') ? v : null
-    this._initRAF()
-  }
-
-  /**
-   * Default handler for resize events.
-   * @callback resizeHandler
-   * @param {ResizeEvent} event
-   *
-   * @type {?resizeHandler}
-   */
-  get onresize () {
-    return this._resize
-  }
-
-  set onresize (v) {
-    if (v === undefined) { return }
-    this.removeEventListener('resize', this._resize)
-    if (typeof v === 'function') {
-      this._resize = v
-      this.addEventListener('resize', this._resize)
-      this._resize({ size: this.size })
-    } else {
-      this._resize = null
+  draw (...args) {
+    if (this._contextLost || !this._draw) {
+      return null
     }
-  }
-
-  /**
-   * Dispose function. Called when element is removed from the DOM.
-   * @type {?function}
-   */
-  get dispose () {
-    return this._dispose
-  }
-
-  set dispose (v) {
-    if (v === undefined) { return }
-    this._dispose = (typeof v === 'function') ? v : null
-  }
-
-  /**
-   * Pixel scaling of canvas. Default is `window.devicePixelRatio`.
-   * @type {number}
-   */
-  get pixelRatio () {
-    return this._pixelRatio
-  }
-
-  set pixelRatio (v) {
-    this._pixelRatio = parseFloat(v) || window.devicePixelRatio
-  }
-
-  /**
-   * WebGL context attributes. Cannot be modified after the context has been retrieved.
-   * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext}
-   * @type {object}
-   */
-  get contextAttributes () {
-    return this._contextAttributes
-  }
-
-  set contextAttributes (opts) {
-    Object.assign(this._contextAttributes, opts)
-  }
-
-  /**
-   * The WebGL context. Attempts creation on first access.
-   * @type {?WebGLRenderingContext}
-   */
-  get gl () {
-    if (this._gl === null) {
-      const gl = this._canvas.getContext('webgl', Object.freeze(this._contextAttributes))
-      if (gl === null) {
-        console.warn('could not initialize WebGL')
-      }
-      this._gl = gl
-    }
-    return this._gl
-  }
-
-  /**
-   * Setup listeners and read contextAttributes from HTML attributes.
-   */
-  connectedCallback () {
-    this.constructor.resizeObserver.observe(this)
-    this._canvas.addEventListener('webglcontextlost', this._ctxLost, false)
-    this._canvas.addEventListener('webglcontextrestored', this._ctxRestored, false)
-
-    const alphaType = getAlphaType(this.getAttribute('alpha'))
-    const powerPreference = getPowerPreference(this.getAttribute('power'))
-
-    const preserveDrawingBuffer = this.hasAttribute('preserve')
-    const failIfMajorPerformanceCaveat = this.hasAttribute('caveat')
-
-    this._contextAttributes = {
-      ...Object.fromEntries(BOOL_ATTRS.map((a) => [a, this.hasAttribute(a)])),
-      ...alphaType,
-      powerPreference,
-      preserveDrawingBuffer,
-      failIfMajorPerformanceCaveat,
-    }
-
-    this._initGL(false)
-  }
-
-  /**
-   * Remove listeners and dispose
-   */
-  disconnectedCallback () {
-    this.constructor.resizeObserver.unobserve(this)
-    this._canvas.removeEventListener('webglcontextlost', this._ctxLost, false)
-    this._canvas.removeEventListener('webglcontextrestored', this._ctxRestored, false)
-
-    if (this.dispose !== null) { this.dispose() }
-  }
-
-  /**
-   * Run setup code and initialize handlers.
-   * @private
-   */
-  _initGL (restored) {
-    if (this.setup === null || this.gl === null) { return }
-    const opts = this.setup(this.gl, restored)
-    if (!opts) { return }
-    this.render = opts.render
-    this.dispose = opts.dispose
-    this.onresize = opts.onresize
-
-    this._initRAF()
-  }
-
-  /**
-   * Start the animation loop.
-   * @private
-   */
-  _initRAF () {
-    if (this.render === null) { return }
-    const render = (time) => {
-      if (this._contextLost) { return }
-      this.render(time)
-      this._requestId = requestAnimationFrame(render)
-    }
-    this._requestId = requestAnimationFrame(render)
-  }
-
-  /**
-   * Create a Blob object representing the image shown in the canvas.
-   * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob}
-   * @param {string} [mimeType] - Image format. Default is `image/png`.
-   * @param {number} [qualityArgument] - A number between 0 and 1.
-   *
-   * @returns {Promise<Blob>} A `Promise` that resolves to the `Blob` object.
-   */
-  toBlob (mimeType, qualityArgument) {
-    return new Promise((resolve, reject) => {
-      try {
-        this._canvas.toBlob(resolve, mimeType, qualityArgument)
-      } catch (e) {
-        reject(e)
-      }
-    })
-  }
-
-  /**
-   * Get a data URI representing the image shown in the canvas.
-   * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL}
-   * @param {string} [type] - Image format. Default is `image/png`.
-   * @param {number} [encoderOptions] - A number between 0 and 1.
-   *
-   * @returns {string} The data URI.
-   */
-  toDataURL (type, encoderOptions) {
-    return this._canvas.toDataURL(type, encoderOptions)
+    return this._draw(...args)
   }
 }
 
---
+++ src/base.js
@@ -0,0 +1,260 @@
+const STYLE = `
+:host([hidden]) { display: none }
+:host, canvas {
+  width: 100%;
+  height: 100%;
+  display: block;
+}`
+
+export class GLCanvasBase extends HTMLElement {
+  /**
+   * Setup Shadow DOM.
+   * @param {object=} contextAttributes - Attributes for the WebGL context.
+   */
+  constructor (contextAttributes) {
+    super()
+
+    this._gl = null
+    this._contextLost = false
+
+    this._pixelRatio = window.devicePixelRatio
+    this._contextAttrs = contextAttributes || {}
+
+    this._ctxLost = (e) => {
+      e.preventDefault()
+      this._contextLost = true
+      this.contextLostCallback()
+    }
+
+    this._ctxRestored = () => {
+      this._contextLost = false
+      this.setupCallback(true)
+    }
+
+    this._canvas = document.createElement('canvas')
+
+    const shadow = this.attachShadow({ mode: 'closed' })
+    shadow.appendChild(this.constructor.style)
+    shadow.appendChild(this._canvas)
+
+    this._shadowRoot = shadow
+  }
+
+  static get booleanContextAttrs () {
+    return {
+      depth: 'depth',
+      stencil: 'stencil',
+      antialias: 'antialias',
+
+      // shortened for brevity
+      desynchronized: 'desync',
+      preserveDrawingBuffer: 'preserve-buffer',
+      failIfMajorPerformanceCaveat: 'require-performance',
+    }
+  }
+
+  static get style () {
+    if (!this._style) {
+      this._style = document.createElement('style')
+      this._style.textContent = STYLE
+    }
+    return this._style
+  }
+
+  static get resizeObserver () {
+    if (!this._resizeObserver) {
+      this._resizeObserver = new ResizeObserver((entries) => {
+        for (const entry of entries) {
+          const target = entry.target
+          const detail = entry.contentRect
+          const evt = new CustomEvent('resize', { detail, cancelable: true })
+          if (target.dispatchEvent(evt)) {
+            target._updateSize(detail)
+            target.resizedCallback(detail)
+          }
+        }
+      })
+    }
+    return this._resizeObserver
+  }
+
+  /**
+   * The WebGL context, if successfully created.
+   * @return {?WebGLRenderingContext}
+   */
+  get gl () {
+    return this._gl
+  }
+
+  /**
+   * Width of the canvas in pixels.
+   * @return {number}
+   */
+  get width () {
+    return this._canvas.width
+  }
+
+  /**
+   * Height of the canvas in pixels.
+   * @return {number}
+   */
+  get height () {
+    return this._canvas.height
+  }
+
+  /**
+   * Pixel scaling of the canvas. Default is `window.devicePixelRatio`.
+   * @return {number}
+   */
+  get pixelRatio () {
+    return this._pixelRatio
+  }
+
+  set pixelRatio (v) {
+    this._pixelRatio = parseFloat(v) || window.devicePixelRatio
+  }
+
+  /**
+   * WebGL context attributes. Cannot be modified after the element is attached.
+   * @return {object}
+   */
+  get contextAttributes () {
+    return this._contextAttrs
+  }
+
+  set contextAttributes (opts) {
+    try {
+      Object.assign(this._contextAttrs, opts || {})
+    } catch (e) {
+      // silently ignore
+    }
+  }
+
+  /**
+   * Parse WebGL context attributes from element attributes.
+   * @private
+   */
+  _parseContextAttrs () {
+    const alphaAttr = this.hasAttribute('alpha') ? this.getAttribute('alpha') : null
+
+    const boolAttrs = Object.entries(this.constructor.booleanContextAttrs)
+      .map(([prop, attr]) => [prop, this.hasAttribute(attr)])
+
+    let powerPreference = this.getAttribute('power-preference')
+
+    switch (powerPreference) {
+      case 'high-performance':
+      case 'low-power':
+        break
+      default:
+        powerPreference = 'default'
+    }
+
+    return {
+      ...Object.fromEntries(boolAttrs),
+      powerPreference,
+      alpha: alphaAttr !== null,
+      premultipliedAlpha: alphaAttr === 'premultiplied',
+    }
+  }
+
+  /**
+   * Update the canvas size.
+   * @private
+   */
+  _updateSize (rect, force) {
+    const rw = parseFloat(rect.width) || this._canvas.clientWidth
+    const rh = parseFloat(rect.height) || this._canvas.clientHeight
+    const width = Math.floor(rw * this.pixelRatio)
+    const height = Math.floor(rh * this.pixelRatio)
+
+    const oldWidth = this._canvas.width
+    const oldHeight = this._canvas.height
+
+    if (force || oldWidth !== width || oldHeight !== height) {
+      this._canvas.width = width
+      this._canvas.height = height
+    }
+  }
+
+  /**
+   * Create WebGL context and setup event listeners.
+   */
+  connectedCallback () {
+    const contextType = this.getAttribute('type') || 'webgl'
+
+    this._contextAttrs = Object.assign(this._parseContextAttrs(), this._contextAttrs)
+    const gl = this._canvas.getContext(contextType, Object.freeze(this._contextAttrs))
+    if (gl === null) { return }
+
+    this._gl = gl
+    this.constructor.resizeObserver.observe(this)
+    this._canvas.addEventListener('webglcontextlost', this._ctxLost)
+    this._canvas.addEventListener('webglcontextrestored', this._ctxRestored)
+
+    if (!this.hasAttribute('role')) {
+      this.setAttribute('role', 'img')
+    }
+
+    this.setupCallback(false)
+  }
+
+  /**
+   * Remove event listeners and dispatch dispose event.
+   */
+  disconnectedCallback () {
+    this.constructor.resizeObserver.unobserve(this)
+    this._canvas.removeEventListener('webglcontextlost', this._ctxLost)
+    this._canvas.removeEventListener('webglcontextrestored', this._ctxRestored)
+
+    this.dispatchEvent(new CustomEvent('dispose'))
+  }
+
+  /**
+   * Run when connected and when the context is restored.
+   */
+  setupCallback (restored) {
+  }
+
+  /**
+   * Run when the context is lost.
+   */
+  contextLostCallback () {
+  }
+
+  /**
+   * Run when the element is resized. Can be cancelled.
+   */
+  resizedCallback (rect) {
+  }
+
+  /**
+   * Create a Blob object representing the image shown in the canvas.
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob}
+   * @param {string=} mimeType - Image format. Default is `image/png`.
+   * @param {number=} qualityArgument - A number between 0 and 1.
+   *
+   * @return {Promise<Blob>} A `Promise` that resolves to the `Blob` object
+   */
+  toBlob (mimeType, qualityArgument) {
+    return new Promise((resolve, reject) => {
+      try {
+        this._canvas.toBlob(resolve, mimeType, qualityArgument)
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }
+
+  /**
+   * Get a data URI representing the image shown in the canvas.
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL}
+   * @param {string=} type - Image format. Default is `image/png`.
+   * @param {number=} encoderOptions - A number between 0 and 1.
+   *
+   * @return {string} The data URI
+   */
+  toDataURL (type, encoderOptions) {
+    return this._canvas.toDataURL(type, encoderOptions)
+  }
+}
